5ff438cab378ecbd77265f3f98e202de47dc2891
mohadkaif122344@gmail.com <mohadkaif122344>
2024-12-01 22:56:09 +0530

    lab program 02

---
+++ question.c
@@ -0,0 +1,17 @@
+// Programs List
+1.	Write a program to calculate simple and 
+    compound interest.
+2.	Write a program to swap values of two variables 
+    with and without using third variable.
+3.	Write a program to display the size of every 
+    data type using “sizeof” operator.
+4.	Write a program to illustrate the use of unary prefix 
+    and postfix increment and decrement operators.
+5.	Write a program to input two numbers and display 
+    the maximum number.
+6.	Write a program to find the largest of three numbers 
+    using ternary operators.
+7.	Write a program to find the roots of quadratic equation.
+8.	Write a program to input name, marks of 5 subjects of a student
+   and display the name of the student, the total marks scored, 
+   percentage scored and the class of result.
